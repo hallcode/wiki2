e7f364a97a450c6175b5c6c02595a1b2b5df0fb2
hallcode <alex@alexhall.dev>
2024-12-10 01:57:34 +0000

    Added external link extension to markdown parser

--- app/Parsers/MarkdownParser.php
+++ app/Parsers/MarkdownParser.php
@@ -5,6 +5,7 @@ namespace App\Parsers;
 use League\CommonMark\Environment\Environment;
 use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
 use League\CommonMark\Extension\Table\TableExtension;
+use League\CommonMark\Extension\ExternalLink\ExternalLinkExtension;
 use League\CommonMark\MarkdownConverter;
 
 class MarkdownParser
@@ -21,6 +22,7 @@ class MarkdownParser
         // Extensions
         $this->environment->addExtension(new CommonMarkCoreExtension());
         $this->environment->addExtension(new TableExtension());
+        $this->environment->addExtension(new ExternalLinkExtension());
     }
 
     /**
